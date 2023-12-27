0e0fbe7e09617973ad0cf45532c5e0808cc5ac55
Jaxon Panter <jmanpanter@gmail.com>
2023-12-27 13:03:58 -0600

    Final operations exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace OperatorExercise
+﻿using System;
+
+namespace OperatorExercise
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //For division, create two integer variables called a and b.
+           
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("Please enter the radius of your circle:");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+
+        //define a method - which performs some functionality. That can 
+        //be used over and over agian
+        public static double AreaOfCircle(double radius)
+        {
+            //this is the scope of the method
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
         }
     }
 }
